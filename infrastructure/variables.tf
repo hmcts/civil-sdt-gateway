0bde38fcad2469b2e841efc2aaa5df661b8b16a4
ridoutp <paul.ridout@cgi.com>
2022-10-28 10:44:53 +0100

    SDT-5: API Management prototype

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -13,21 +13,21 @@ variable "subscription" {}
 variable "deployment_namespace" {}
 
 variable "common_tags" {
-  type = "map"
+  type = map(string)
 }
 
 # New variables
 variable "my_product" {
-  type = "string"
+  type = string
   default = "civil"
 }
 
 variable "my_component" {
-  type = "string"
+  type = string
   default = "sdt-gateway"
 }
 
 variable "my_subscription_id" {
-  description = "Subscription id for the APIM service.  Set to environment specific value by pipeline."
-  type = "string"
+  description = "Subscription id.  Set to environment specific value by pipeline."
+  type = string
 }
