4f4953aa23bab817ea790fca369310ab232d770d
ridoutp <paul.ridout@cgi.com>
2022-10-28 13:39:16 +0100

    SDT-5: API Management prototype

--- infrastructure/apim.tf
+++ infrastructure/apim.tf
@@ -6,7 +6,4 @@ module "api_mgmt_product" {
   subscription_required = "false"
   api_mgmt_name         = "cft-api-mgmt-${var.env}"
   api_mgmt_rg           = "cft-${var.env}-network-rg"
-  providers             = {
-    azurerm = azurerm
-  }
 }
